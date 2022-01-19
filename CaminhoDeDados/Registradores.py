6cf44d1bfdc30567aa96f3cf05e3b7b71e25c9d7
Israel Louback <israel.louback@engenharia.ufjf.br>
2022-01-19 17:21:27 -0300

    padrao de implementacao

--- CaminhoDeDados/Registradores.py
+++ CaminhoDeDados/Registradores.py
@@ -24,14 +24,14 @@ class zero:
         return cls.id
 
 
-class s0:
+class v0:
     bits: list = []  # Lista utilizada para representar os 32 bits do registrador
-    id = '010000'
+    id = '000010'
 
     @classmethod
     def inicializar(cls):
         """
-        Método para colocar todos os bits do registrador $s0 com o valor '0'
+        Método para colocar todos os bits do registrador $v0 com o valor '0'
         :return: O método não possui retorno
         """
         for contador in range(32):
@@ -94,4 +94,45 @@ class s0:
         Retorna o campo 'id' do registrador
         :return: id -> número do registrador
         """
+        return cls.id
+
+
+class s0:
+    bits: list = []
+    id = '010000'
+
+    @classmethod
+    def inicializar(cls):
+        for contador in range(32):
+            cls.bits.insert(0, '0')
+
+    @classmethod
+    def set_bits(cls, bit_inicial, bit_final, sequencia_de_bits):
+        if type(sequencia_de_bits) != list:
+            sequencia_de_bits = list(sequencia_de_bits)
+        cont: int = 0
+        for valor in range(bit_inicial, bit_final + 1):
+            cls.bits[valor] = sequencia_de_bits[cont]
+            cont += 1
+
+    @classmethod
+    def get_bits(cls, bit_inicial, bit_final):
+        return cls.bits[bit_inicial:(bit_final + 1)]
+
+    @classmethod
+    def set(cls, bits):
+        if type(bits) == list and len(bits) == 32:
+            cls.bits = bits
+        elif len(bits) == 32:
+            for valor in bits:
+                cls.bits.insert(0, valor)
+        else:
+            print("Quantidade de bits tem de ser igual a 32!\n")
+
+    @classmethod
+    def get(cls):
+        return cls.bits
+
+    @classmethod
+    def get_id(cls):
         return cls.id
