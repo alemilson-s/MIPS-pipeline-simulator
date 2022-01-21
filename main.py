9fac8a73d1c704122a705fe707220e26df30a555
Israel Louback <israel.louback@engenharia.ufjf.br>
2022-01-21 10:02:20 -0300

    remocao do enter(\n) na leitura de arquivo

--- main.py
+++ main.py
@@ -7,14 +7,15 @@ class Arquivo:
         linhas: list = []
         with open(path) as arquivo:
             for linha in arquivo:
-                linhas.append(linha)
+                linhas.append(linha.replace("\n",""))
         return linhas
 
 
 if __name__ == "__main__":
-    caminho_arquivo = "C:\\Users\\ALEMILSON\\OneDrive\\Imagens\\PROVA2EST\\testeLeitura.txt"
+    caminho_arquivo = "C:\\Users\\Israel Louback\\Documents\\teste.txt"
 
     linhas_arquivo = Arquivo.read(caminho_arquivo)
 
     for linha in linhas_arquivo:
         MemoriaDeInstrucoes.MemoriaDeInstrucoes.inserir_instrucao(linha)
+        print(linha)
