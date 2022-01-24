f40e5a72374de607659eca0c3dffdc0caefa5f80
ALEMILSON F. SILVA <alemilson@ice.ufjf.br>
2022-01-24 11:16:20 -0300

    Alteração de ordem das classes em Pipeline/Registradores

--- CaminhoDeDados/Pipeline/Registradores.py
+++ CaminhoDeDados/Pipeline/Registradores.py
@@ -224,6 +224,77 @@ class id_ex:
         return cls.readRegister_2
 
 
+class ex_mem:
+    # Linhas de controle do estágio de escrita do resultado
+    RegWrite = None
+    MemtoReg = None
+
+    dataMemory = None
+    alu = None
+    mux = None
+
+    @classmethod
+    def setRegWrite(cls, bit):
+        cls.RegWrite = bit
+
+    @classmethod
+    def getRegWrite(cls):
+        return cls.RegWrite
+
+    @classmethod
+    def setMemtoReg(cls, bit):
+        cls.MemtoReg = bit
+
+    @classmethod
+    def getMemtoReg(cls):
+        return cls.MemtoReg
+
+    @classmethod
+    def setDataMemory(cls, bits):
+        if type(bits) == list and len(bits) == 32:
+            cls.dataMemory = bits.copy()
+        elif len(bits) == 32:
+            cls.dataMemory.clear()
+            for valor in bits:
+                cls.dataMemory.insert(0, valor)
+        else:
+            print("Quantidade de bits tem de ser igual a 32!\n")
+
+    @classmethod
+    def getDataMemory(cls):
+        return cls.dataMemory
+
+    @classmethod
+    def setAlu(cls, bits):
+        if type(bits) == list and len(bits) == 32:
+            cls.alu = bits.copy()
+        elif len(bits) == 32:
+            cls.alu.clear()
+            for valor in bits:
+                cls.alu.insert(0, valor)
+        else:
+            print("Quantidade de bits tem de ser igual a 32!\n")
+
+    @classmethod
+    def getAlu(cls):
+        return cls.alu
+
+    @classmethod
+    def setMux(cls, bits):
+        if type(bits) == list and len(bits) == 32:
+            cls.mux = bits.copy()
+        elif len(bits) == 32:
+            cls.mux.clear()
+            for valor in bits:
+                cls.mux.insert(0, valor)
+        else:
+            print("Quantidade de bits tem de ser igual a 32!\n")
+
+    @classmethod
+    def getMux(cls):
+        return cls.mux
+
+
 class mem_wb:
     # Linas de controledo estágio de acesso à memória
     MemRead = None
@@ -315,74 +386,3 @@ class mem_wb:
     def getAlu(cls):
 
         return cls.alu
-
-
-class ex_mem:
-    # Linhas de controle do estágio de escrita do resultado
-    RegWrite = None
-    MemtoReg = None
-
-    dataMemory = None
-    alu = None
-    mux = None
-
-    @classmethod
-    def setRegWrite(cls, bit):
-        cls.RegWrite = bit
-
-    @classmethod
-    def getRegWrite(cls):
-        return cls.RegWrite
-
-    @classmethod
-    def setMemtoReg(cls, bit):
-        cls.MemtoReg = bit
-
-    @classmethod
-    def getMemtoReg(cls):
-        return cls.MemtoReg
-
-    @classmethod
-    def setDataMemory(cls, bits):
-        if type(bits) == list and len(bits) == 32:
-            cls.dataMemory = bits.copy()
-        elif len(bits) == 32:
-            cls.dataMemory.clear()
-            for valor in bits:
-                cls.dataMemory.insert(0, valor)
-        else:
-            print("Quantidade de bits tem de ser igual a 32!\n")
-
-    @classmethod
-    def getDataMemory(cls):
-        return cls.dataMemory
-
-    @classmethod
-    def setAlu(cls, bits):
-        if type(bits) == list and len(bits) == 32:
-            cls.alu = bits.copy()
-        elif len(bits) == 32:
-            cls.alu.clear()
-            for valor in bits:
-                cls.alu.insert(0, valor)
-        else:
-            print("Quantidade de bits tem de ser igual a 32!\n")
-
-    @classmethod
-    def getAlu(cls):
-        return cls.alu
-
-    @classmethod
-    def setMux(cls, bits):
-        if type(bits) == list and len(bits) == 32:
-            cls.mux = bits.copy()
-        elif len(bits) == 32:
-            cls.mux.clear()
-            for valor in bits:
-                cls.mux.insert(0, valor)
-        else:
-            print("Quantidade de bits tem de ser igual a 32!\n")
-
-    @classmethod
-    def getMux(cls):
-        return cls.mux
