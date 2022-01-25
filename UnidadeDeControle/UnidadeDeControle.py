cfe4468779b5a17cca71474586bfe50a073f3e2b
ALEMILSON F. SILVA <alemilson@ice.ufjf.br>
2022-01-25 13:00:23 -0300

    Correção Registradores Pipeline

---
+++ UnidadeDeControle/UnidadeDeControle.py
@@ -0,0 +1,13 @@
+class Control:
+    pass
+
+
+class FowardingUnit:
+    readRegister_1 = None
+    readRegister_2 = None
+    instruction_16_20_or_11_15_mem_wb = None
+    instruction_16_20_or_11_15_ex_men = None
+
+
+class HazardDetectionUnit:
+    pass
